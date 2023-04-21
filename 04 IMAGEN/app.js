2789c75aeecbf695794b11e0ab53e8fd2a80319e
Nicole <you@example.com>
2023-04-21 10:51:14 -0600

    ACABE

---
+++ 04 IMAGEN/app.js
@@ -0,0 +1,14 @@
+const uno=()=>{
+    let img= document.querySelector("#imagen");
+    
+    img.src="Luis.jpg";
+    }
+    const dos=()=>{
+    let img= document.querySelector("#imagen");
+    img.src="Luisin.jpg";
+    }
+    const tres=()=>{
+    let img= document.querySelector("#imagen");
+    img.src="Luisito.jpg";
+    
+    }    
