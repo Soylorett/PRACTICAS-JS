5db2ae817bba8801218006bc7029e8e06fb692da
Nicole <nicole@14.com>
2023-04-26 09:02:15 -0600

    TERMINE

--- 06 STILE/app.js
+++ 06 STILE/app.js
@@ -3,14 +3,12 @@ const cambiar=()=>{
     let colorFondo=document.querySelector("#color").value;
     let colorTex=document.querySelector("#colort").value;
     let figura=document.querySelector("#figura").value;
-    let texto=document.querySelector("#texto").value;
     let br= (figura=="1")?"0%":"100%"
+    let texto=document.querySelector("#texto").value;
     let ancho=document.querySelector("#ancho").value;
     let alto=document.querySelector("#alto").value;
-    document.querySelector("#vancho").innerHTML
-    document.querySelector("#valto").innerHTML
     div.style.width=ancho+"px"
-    div.style.width=alto+"px"
+    div.style.height=alto+"px"
     div.innerHTML=texto
     div.style.borderRadius=br
     div.style.background=colorFondo;
