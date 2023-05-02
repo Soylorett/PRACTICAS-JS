de2535e5bfc104d034e790a4d4805e90578f4f1b
Nicole <lorettrodriguez1426@gmail.com>
2023-05-01 19:18:09 -0500

    ACABEEE NICOLE

---
+++ 09 RAIZ CUADRADA/app.js
@@ -0,0 +1,15 @@
+const generar = () => {
+    let n = parseInt(document.querySelector("#n").value);
+    document.querySelector("#vn").innerHTML = n
+    let div = document.querySelector("#res");
+    div.innerHTML = "<img src='spinner.gif' width='200px' height='200px'>";
+    setTimeout(() => {
+    let r = Math.sqrt(n);
+    div.innerHTML = r.toFixed(2);
+    }, 2000);
+    const borrar = () => {
+    }
+    let borrarT = document.querySelector("#res").innerHTML = "";
+    }
+
+
