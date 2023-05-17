dc9212355e11ce802a110f2d7ec989215681fe01
Nicole Loreta <112711690+Soylorett@users.noreply.github.com>
2023-05-17 09:32:12 -0600

    NICOLE YA ACANE

--- 16 EXAMEN 2/app.js
+++ 16 EXAMEN 2/app.js
@@ -1,29 +1,99 @@
-let d =document.querySelector("#d")
-let m = document.querySelector("#m")
+let d=document.querySelector("#dia")
+let vdia=document.querySelector("#vdia")
+let m=document.querySelector("#mes")
+let fecha=document.querySelector("#fecha")
+let signo=document.querySelector("#signo")
 
-d.oninput=()=>{signo()}
-m.onchange=()=>{signo()}
+d.oninput=()=>{
+    vdia.innerHTML=d.value
+    lafecha() 
+}
+m.onchange=()=>{
+    lafecha()
+}
+const lafecha=()=>{
 
-const signo=()=> {
-document.querySelector("#vd").innerHTML=d.value
-document.querySelector("#fecha").innerHTML=`FECHA ${d.value} de ${m.value}`
-let di=parseInt(d.value)
-let signo=""
-switch(m.value){
-    case"Enero": signo=(di>=20)?"CAPRICORNIO":"ACUARIO";break;
-    case"Febrero": signo=(di>=20)?"ACUARIO":"PISCIS";break;
-    case"Marzo": signo=(di>=20)?"PISCIS":"ARIES";break;
-    case"Abrl": signo=(di>=20)?"ARIES":"TAURO";break;
-    case"Mayo": signo=(di>=20)?"TAURO":"GEMINIS";break;
-    case"Junio": signo=(di>=20)?"GEMINIS":"CANCER";break;
-    case"Julio": signo=(di>=20)?"CANCER":"LEO";break;
-    case"Agosto": signo=(di>=20)?"LEO":"VIRGO";break;
-    case"Septiembre": signo=(di>=20)?"VIRGO":"LIBRA";break;
-    case"Octubre": signo=(di>=20)?"LIBRA":"ESCORPION";break;
-    case"Noviembre": signo=(di>=20)?"ESCORPION":"SAGITARIO";break;
-    case"Diciembre": signo=(di>=20)?"SAGITARIO":"CAPRICORNIO";break;
+    
+    dia=parseInt(d.value)
+    mes=parseInt(m.value)
+    var me=""
+
+if(mes==1){
+me="ENERO"
+}
+else if(mes==2){
+    me="FEBRERO"
+}
+else if(mes==3){
+me="MARZO"
+}
+else if(mes==4){
+    me="ABRIL"
+}
+else if(mes==5){
+    me="MAYO"
+}
+else if(mes==6){
+    me="JUNIO"
+ }
+else if(mes==7){
+    me="JULIO"
+}
+else if(mes==8){
+me="AGOSTO"
+}
+else if(mes==9){
+ me="SEPTIEMBRE"
+}
+else if(mes==10){
+    me="OCTUBRE"
+}
+else if(mes==11){
+    me="NOVIEMBRE"
+}
+else if(mes==12){
+    me="DICIEMBRE"
+    }
+    fecha.innerHTML="FECHA "+d.value+" DE "+me
+if((dia>=20 && mes==1)||(dia<=18 && mes==2)){
+    signo.innerHTML="ACUARIO"
+}
+else if((dia>=19 && mes==2)||(dia<=20 && mes==3)){
+    signo.innerHTML="PISCIS"
+}
+else if((dia>=21 && mes==3)||(dia<=19 && mes==4)){
+    signo.innerHTML="ARIES"
+}
+else if((dia>=20 && mes==4)||(dia<=20 && mes==5)){
+    signo.innerHTML="TAURO"
+}
+else if((dia>=21 && mes==5)||(dia<=20 && mes==6)){
+    signo.innerHTML="GEMINIS"
+}
+else if((dia>=21 && mes==6)||(dia<=22 && mes==7)){
+    signo.innerHTML="CANCER"
+}
+else if((dia>=23 && mes==7)||(dia<=22 && mes==8)){
+    signo.innerHTML="LEO"
+}
+else if((dia>=23 && mes==8)||(dia<=22 && mes==9)){
+    signo.innerHTML="VIRGO"
+}
+else if((dia>=23 && mes==9)||(dia<=22 && mes==10)){
+    signo.innerHTML="LIBRA"
+}
+else if((dia>=23 && mes==10)||(dia<=21 && mes==11)){
+    signo.innerHTML="ESCORPIO"
+}
+else if((dia>=22 && mes==11)||(dia<=21 && mes==12)){
+    signo.innerHTML="SAGITARIO"
+}
+else if((dia>=22 && mes==12)||(dia<=19 && mes==1)){
+    signo.innerHTML="CAPRICORNIO"
+
+
 
 }
-document.querySelector("#signo").innerHTML=signo;
+
 
 }
